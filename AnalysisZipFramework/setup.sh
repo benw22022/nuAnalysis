12d68fa7c434a4f8cf6178bb9f6a591e63fa2633
bewilson <benjamin.james.wilson@cern.ch>
2026-07-10 14:50:08 +0100

    suppress RDF info message

--- AnalysisZipFramework/setup.sh
+++ AnalysisZipFramework/setup.sh
@@ -7,4 +7,7 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_109/x86_64-el9-gcc15-opt/setup.sh
 ## for CERNBOX eos access
 export EOS_MGM_URL=root://eosuser.cern.ch
 
+## Supress ROOT RDF snapshot info messages
+export ROOT_RDF_SNAPSHOT_INFO=0
+
 echo "Setup completed!"
