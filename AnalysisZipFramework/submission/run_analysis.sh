08d68042f2b8a79440489270485af76ef68d3f13
bewilson <benjamin.james.wilson@cern.ch>
2026-07-10 14:50:42 +0100

    add x509 proxy

--- AnalysisZipFramework/submission/run_analysis.sh
+++ AnalysisZipFramework/submission/run_analysis.sh
@@ -4,8 +4,11 @@
 # Source LCG view
 source /cvmfs/sft.cern.ch/lcg/views/LCG_109/x86_64-el9-gcc15-opt/setup.sh
 
+## Supress ROOT RDF snapshot info messages
+export ROOT_RDF_SNAPSHOT_INFO=0
+
 # Move to the build directory where the binary and config/ live
-cd /afs/cern.ch/work/b/bewilson/AnalyisZipFramework/build
+cd /home/ppd/bewilson/work/nuAnalysis/build
 
 echo "Starting analysis for run $1"
 echo "Output: $2"
--- AnalysisZipFramework/submission/run_submission.sh
+++ AnalysisZipFramework/submission/run_submission.sh
@@ -1,12 +1,13 @@
 # Create the logs directory first (HTCondor won't create it)
 mkdir -p logs
-mkdir -p /eos/home-b/bewilson/analysis_output-v3
+mkdir -p /home/ppd/bewilson/work/nuAnalysis/AnalysisZipFramework/submission/analysis_output-noVetoNu
 
 # Dry run to check expansion
 # condor_submit -dry-run - analysis.sub outputDir=/eos/home-b/bewilson/analysis_output
 
 # # Real submission
-condor_submit analysis.sub outputDir=/eos/home-b/bewilson/analysis_output-v3
+voms-proxy-init -valid 96:0 -out ./x509up_u$(id -u)
+condor_submit analysis.sub outputDir=/home/ppd/bewilson/work/nuAnalysis/AnalysisZipFramework/submission/analysis_output-noVetoNu
 
 # # Monitor
 # condor_q
